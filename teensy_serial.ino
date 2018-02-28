136890be818e77fddb702b3b42393983a0e53deb
paul wisehart <paul@oldcode.org>
2018-02-28 09:13:03 -0500

    nothing to see here

--- teensy_serial.ino
+++ teensy_serial.ino
@@ -1,19 +1,14 @@
 int ledPin = 13;                 // LED connected to digital pin 13
 
-void setup()
-{
+void setup() {
   pinMode(ledPin, OUTPUT);      // sets the digital pin as output
 
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
-
-
-
-void loop()
-{
-  Serial.println("hey dood!");
+void loop() {
+  Serial.println("|||+-+-+-+->>>");
   digitalWrite(ledPin, HIGH);   // sets the LED on
   delay(1000);                  // waits for a second
   digitalWrite(ledPin, LOW);    // sets the LED off
