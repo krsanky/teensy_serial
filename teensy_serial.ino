99cfc1d35990d0be4694d893bc2de7fe37aaebbe
paul wisehart <paul@oldcode.org>
2018-02-28 14:03:22 -0500

    cleanup

--- teensy_serial.ino
+++ teensy_serial.ino
@@ -1,9 +1,3 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-  This example code is in the public domain.
- */
-
 void setup() {                
 	// initialize the digital pin as an output.
 	// Pin 13 has an LED connected on most Arduino boards:
